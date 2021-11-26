a9e4c685906714cd7a4626f9fc895e477af2fd27
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-11-26 18:46:24 -0300

    Exercices - return the bigger of 3  numbers.

---
+++ fundamentos/bloco-4-introducao-js-e-logica-programacao/dia-1-primeiros-passos/exercícios/return-the-bigger-of-3.js
@@ -0,0 +1,13 @@
+let a = 3;
+let b = 3;
+let c = 3;
+
+if (a > b && a > c) {
+  console.log(a);
+} else if (b > a && b > c) {
+  console.log(b);
+} else if (c > a && c > b) {
+  console.log(c);
+} else {
+  console.log('The numbers are equal');
+}
