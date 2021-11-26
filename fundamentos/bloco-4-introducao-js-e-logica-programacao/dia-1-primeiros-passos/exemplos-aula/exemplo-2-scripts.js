e1507158d4d52163d942be5bf7ad6fec6e4a2a02
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-11-26 15:31:17 -0300

    Exemple 2 - variable types.

---
+++ fundamentos/bloco-4-introducao-js-e-logica-programacao/dia-1-primeiros-passos/exemplos-aula/exemplo-2-scripts.js
@@ -0,0 +1,9 @@
+let base = 5;
+let height = 8;
+let area = base * height;
+
+console.log(`A área é de ${area}.`);
+
+let perimeter = 2 * base + 2 * height;
+
+console.log(`O perímetro é de ${perimeter}`);
