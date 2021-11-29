d095513c218e438b333e682c0f6b0c0a731c0a62
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-11-29 16:37:34 -0300

    Exercice 8, day 2, block 4.

---
+++ fundamentos/bloco-4-introducao-js-e-logica-programacao/dia-2-arrays-loop-for/exercicios/exercicio-8.js
@@ -0,0 +1,8 @@
+let maxNumberOfSequence = 25;
+let numberSequence = [];
+
+for (let index = 0; index < maxNumberOfSequence; index++) {
+  numberSequence[index] = index + 1;
+}
+
+console.log(numberSequence);
