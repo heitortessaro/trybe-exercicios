8f95376c0a1fbdbd585c793380b03cc974e2051f
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-02 09:51:44 -0300

    Exercicio 6 funcoes, dia 4 (objetos e funcoes), bloco 4.

---
+++ fundamentos/bloco-4-introducao-js-e-logica-programacao/dia-4-objetos-funcoes/exercicios/exercicio-6-funcoes.js
@@ -0,0 +1,13 @@
+// Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+
+function returnSumOfNumbers(max) {
+  let sum = 0;
+  for (let index = 1; index <= max; index += 1) {
+    sum += index;
+  }
+  return sum;
+}
+
+let n = 5;
+
+console.log(returnSumOfNumbers(n));
