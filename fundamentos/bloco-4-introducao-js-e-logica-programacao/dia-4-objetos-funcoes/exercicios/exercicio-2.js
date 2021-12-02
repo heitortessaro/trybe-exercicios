a3c96302fe4e2dc6f23cc6c3f31e95ecb0dea629
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-02 07:55:24 -0300

    Exercicio 2 - parte 2 e 3, dia 4 (objetos e funcoes), bloco 4.

--- fundamentos/bloco-4-introducao-js-e-logica-programacao/dia-4-objetos-funcoes/exercicios/exercicio-2.js
+++ fundamentos/bloco-4-introducao-js-e-logica-programacao/dia-4-objetos-funcoes/exercicios/exercicio-2.js
@@ -16,3 +16,23 @@ let indexListaFavoritos = 0;
 console.log(
   `O livro favorito de ${leitor.nome} se chama ${leitor.livrosFavoritos[indexListaFavoritos].titulo}.`
 );
+
+leitor.livrosFavoritos[1] = {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editor: 'Rocco',
+};
+
+// console.table(leitor);
+console.log('----------------------');
+
+console.log(
+  `O segundo livro favorito de ${leitor.nome} se chama ${
+    leitor.livrosFavoritos[indexListaFavoritos + 1].titulo
+  }.`
+);
+
+console.log('----------------------');
+console.log(
+  `${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos.`
+);
