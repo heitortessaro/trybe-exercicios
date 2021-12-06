5afb48a6471af5b46d1a7812b6a56e4ca10c9bba
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-06 18:54:39 -0300

    Resolution of part 4.

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-elementos/exercicio/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-elementos/exercicio/script.js
@@ -14,7 +14,12 @@ let centerSection = document.createElement('section');
 centerSection.classList.add('center-content');
 main.appendChild(centerSection);
 
-// Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+// 4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+let para = document.createElement('p');
+para.innerText =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+centerSection.appendChild(para);
+
 // Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
 // Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
