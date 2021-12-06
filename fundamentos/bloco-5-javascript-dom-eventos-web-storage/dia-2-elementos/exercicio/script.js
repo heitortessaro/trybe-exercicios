c2f3dbd09c2f9c61269de28ac4cd771de1218050
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-06 18:57:40 -0300

    Resolution of parts 5 and 6.

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-elementos/exercicio/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-elementos/exercicio/script.js
@@ -20,8 +20,16 @@ para.innerText =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 centerSection.appendChild(para);
 
-// Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
-// Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+// 5 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+let leftSection = document.createElement('section');
+leftSection.classList.add('left-content');
+main.appendChild(leftSection);
+
+// 6 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+let rightSection = document.createElement('section');
+rightSection.classList.add('right-content');
+main.appendChild(rightSection);
+
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 // Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
