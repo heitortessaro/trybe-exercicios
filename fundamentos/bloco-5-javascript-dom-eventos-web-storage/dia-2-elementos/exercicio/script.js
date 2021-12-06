b60efe926602e62cc4316e29f194de0c3790964e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-06 19:23:51 -0300

    Resolution of bonus parts 3 and 4.

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-elementos/exercicio/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-elementos/exercicio/script.js
@@ -73,6 +73,10 @@ h1.classList.add('title');
 // add at the moment of creation above
 
 // B3 - Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+main.removeChild(leftSection);
+
 // B4 - Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+rightSection.style.marginRight = 'auto';
+
 // B5 - Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
 // B6 - Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
