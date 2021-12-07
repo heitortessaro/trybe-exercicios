41e80f67e7a665146aa6ee5fccf551d212f54562
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-07 09:35:03 -0300

    Exercises day 2 form block 5. Test toUpperCase() to help classmate

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-elementos/exercicio/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-elementos/exercicio/script.js
@@ -18,6 +18,7 @@ main.appendChild(centerSection);
 let para = document.createElement('p');
 para.innerText =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+para.innerText = para.innerText.toUpperCase();
 centerSection.appendChild(para);
 
 // 5 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
