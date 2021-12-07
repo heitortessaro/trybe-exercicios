d2f39ea39efe08e5ba161690f4f36d2b82e35a6d
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-07 18:50:14 -0300

    Exercise 2 and 3, from day 3 of block 5.

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/exemplos/exemplo-1/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/exemplos/exemplo-1/main.js
@@ -28,7 +28,10 @@ function changeClass(event) {
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-
+input.addEventListener('keyup', function () {
+  let classElement = document.getElementsByClassName('tech')[0];
+  classElement.innerText = input.value;
+});
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
