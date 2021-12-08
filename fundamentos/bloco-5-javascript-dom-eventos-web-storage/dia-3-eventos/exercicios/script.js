038f3f7eed30fc63d6c31d0cfdac659d06cce869
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-08 08:01:22 -0300

    Part 1 - dynamically adds the days of the month. Adds classes for Fridays and holidays.

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-3-eventos/exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-3-eventos/exercicios/script.js
@@ -22,3 +22,30 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+function createDaysOfTheMonth(daysList, holidayList, fridayList) {
+  const ulDays = document.getElementById('days');
+  for (let index = 0; index < daysList.length; index += 1) {
+    let tempLi = document.createElement('li');
+    tempLi.innerText = daysList[index];
+    // tempLi.classList.add('day');
+    // console.log(holidayList.include() daysList[index]);
+    if (holidayList.includes(daysList[index])) {
+      tempLi.classList.add('holiday');
+    }
+    if (fridayList.includes(daysList[index])) {
+      tempLi.classList.add('friday');
+    }
+    ulDays.appendChild(tempLi);
+  }
+}
+
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+const dezHolidayList = [24, 25, 31];
+const dezFridayList = [4, 11, 18, 25];
+
+createDaysOfTheMonth(dezDaysList, dezHolidayList, dezFridayList);
+
+// console.log(dezDaysList.includes(dezHolidayList[0]));
