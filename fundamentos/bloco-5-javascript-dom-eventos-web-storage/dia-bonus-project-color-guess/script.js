ccc0df69ad94523bff1060b8982637f5c3102921
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2021-12-13 08:35:51 -0300

    Add sub elements to colorSec. The mainColor contains the rgb-color objective. The asideColors contains the color divs that the user should choose.

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-bonus-project-color-guess/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-bonus-project-color-guess/script.js
@@ -6,19 +6,14 @@ const espacoJogo = document.getElementById('espaco-jogo');
 
 // Requisito 2
 function randomColor(comRGB) {
-  const randRed = Math.floor(256 * Math.random());
-  const randGreen = Math.floor(256 * Math.random());
-  const randBlue = Math.floor(256 * Math.random());
-  if (comRGB) {
-    return `rgb(${randRed}, ${randGreen}, ${randBlue})`;
-  }
-  return `(${randRed}, ${randGreen}, ${randBlue})`;
+ const randRed = Math.floor(256 * Math.random());
+ const randGreen = Math.floor(256 * Math.random());
+ const randBlue = Math.floor(256 * Math.random());
+ if (comRGB) {
+  return `rgb(${randRed}, ${randGreen}, ${randBlue})`;
+ }
+ return `(${randRed}, ${randGreen}, ${randBlue})`;
 }
-
-const rgbColorText = document.createElement('p');
-rgbColorText.id = 'rgb-color';
-rgbColorText.innerText = randomColor(false);
-espacoJogo.appendChild(rgbColorText);
 
 // Requisito 5
 // método string includes: https://www.w3schools.com/jsref/jsref_includes.asp
@@ -36,12 +31,12 @@ resposta.innerText = 'Escolha uma cor';
 espacoJogo.appendChild(resposta);
 
 function verificaCor(event) {
-  if (event.target.classList.contains('escolha-correta')) {
-    resposta.innerText = 'Acertou!';
-    incrementaPontuacao();
-    return;
-  }
-  resposta.innerText = 'Errou! Tente novamente!';
+ if (event.target.classList.contains('escolha-correta')) {
+  resposta.innerText = 'Acertou!';
+  incrementaPontuacao();
+  return;
+ }
+ resposta.innerText = 'Errou! Tente novamente!';
 }
 
 // Requisito 3 - 4 - 5
@@ -49,20 +44,31 @@ const colorsSec = document.createElement('section');
 colorsSec.id = 'secao-cores-apresentadas';
 espacoJogo.appendChild(colorsSec);
 
+const mainColor = document.createElement('div');
+colorsSec.appendChild(mainColor);
+
+const asideColors = document.createElement('aside');
+colorsSec.appendChild(asideColors);
+
+const rgbColorText = document.createElement('p');
+rgbColorText.id = 'rgb-color';
+rgbColorText.innerText = randomColor(false);
+mainColor.appendChild(rgbColorText);
+
 function adicionaDivsComCor(quantidade) {
-  const correta = Math.floor(6 * Math.random());
-  for (let index = 0; index < quantidade; index += 1) {
-    const tempDiv = document.createElement('div');
-    tempDiv.classList.add('ball');
-    if (index === correta) {
-      tempDiv.style.backgroundColor = 'rgb'.concat(rgbColorText.innerText);
-      tempDiv.classList.add('escolha-correta');
-    } else {
-      tempDiv.style.backgroundColor = randomColor(true);
-    }
-    tempDiv.addEventListener('click', verificaCor);
-    colorsSec.appendChild(tempDiv);
+ const correta = Math.floor(6 * Math.random());
+ for (let index = 0; index < quantidade; index += 1) {
+  const tempDiv = document.createElement('div');
+  tempDiv.classList.add('ball');
+  if (index === correta) {
+   tempDiv.style.backgroundColor = 'rgb'.concat(rgbColorText.innerText);
+   tempDiv.classList.add('escolha-correta');
+  } else {
+   tempDiv.style.backgroundColor = randomColor(true);
   }
+  tempDiv.addEventListener('click', verificaCor);
+  asideColors.appendChild(tempDiv);
+ }
 }
 
 const numeroOpcoes = 6;
@@ -70,16 +76,16 @@ adicionaDivsComCor(numeroOpcoes);
 
 // Requisito 6
 function removeDivs() {
-  while (colorsSec.lastChild) {
-    colorsSec.lastChild.remove();
-  }
+ while (asideColors.lastChild) {
+  asideColors.lastChild.remove();
+ }
 }
 
 function gerenciaJogo() {
-  rgbColorText.innerText = randomColor(false);
-  removeDivs();
-  adicionaDivsComCor(numeroOpcoes);
-  resposta.innerText = 'Escolha uma cor';
+ rgbColorText.innerText = randomColor(false);
+ removeDivs();
+ adicionaDivsComCor(numeroOpcoes);
+ resposta.innerText = 'Escolha uma cor';
 }
 
 const iniciaJogo = document.createElement('button');
@@ -100,6 +106,6 @@ score.innerText = '0';
 scoreInfo.appendChild(score);
 
 function incrementaPontuacao() {
-  const pontuacaoAtual = parseInt(score.innerText, 10);
-  score.innerText = pontuacaoAtual + 3;
+ const pontuacaoAtual = parseInt(score.innerText, 10);
+ score.innerText = pontuacaoAtual + 3;
 }
