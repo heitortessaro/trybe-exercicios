6bcab0d09be10c190a7ea6d6a590717299dfe997
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-01-04 15:49:28 -0300

    Added eventlistener to the link and the checkbox using preventDefault.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exemplos/exemplo-2/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exemplos/exemplo-2/script.js
@@ -2,3 +2,16 @@
 const INPUT_TEXT = document.querySelector('#input-text');
 const INPUT_CHECKBOX = document.querySelector('#input-checkbox');
 const HREF_LINK = document.querySelector('#href');
+
+function handleHrefClick(event) {
+ event.preventDefault();
+ return;
+}
+
+function handleInputCheckbox(event) {
+ event.preventDefault();
+ return;
+}
+
+HREF_LINK.addEventListener('click', handleHrefClick);
+INPUT_CHECKBOX.addEventListener('click', handleInputCheckbox);
