0cd2600dae53bc99705e2306e9838eee69da109e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-01-05 18:27:12 -0300

    Alterações para tentar fazer o botão funcionar.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/aula/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/aula/script.js
@@ -2,6 +2,9 @@ let myModal = new bootstrap.Modal(document.getElementById('myModal'), options);
 
 let button = document.getElementById('nao-clicar');
 
+console.log('teste');
+
 button.addEventListener('click', function () {
+ alert('teste');
  myModal.show();
 });
