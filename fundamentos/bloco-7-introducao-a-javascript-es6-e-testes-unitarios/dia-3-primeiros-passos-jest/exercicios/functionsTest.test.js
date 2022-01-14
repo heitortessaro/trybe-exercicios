8c8ae201fe6220ca3b4b1d12f05109cbb6403fd1
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-01-14 19:25:02 -0300

    Exercicios 2 e 3 Jest.

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-jest/exercicios/functionsTest.test.js
@@ -0,0 +1,61 @@
+// sum.js
+const sum = require('./sum');
+
+// myRemove.js
+const myRemove = require('./myRemove');
+
+// myFizzBuzz.js
+const myFizzBuzz = require('./myFizzBuzz')
+
+describe("It tests the sum function:", () => {
+
+    it('The sum of 4 and 5 is 9', () => {
+        expect(sum(4, 5)).toBe(9);
+    });
+
+    it('The sum of 0 and 0 is 0', () => {
+        expect(sum(0, 0)).toBe(0);
+    });
+
+    it('Verify if error is threw', () => {
+        expect(() => { sum(4, '5') }).toThrow();
+    });
+
+    it('Verify if error is threw', () => {
+        expect(() => { sum(4, '5') }).toThrowError(new Error('parameters must be numbers'));
+    });
+});
+
+describe('It tests the myRemove function:', () => {
+
+    it('Test if iten 3 is removed', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    });
+    it('Test if the vector is not returned equal', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    });
+    it('Verify if the output and input vector are the same', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
+
+});
+
+describe('It test the myFizzBuzz function:', () => {
+
+    it('Tests a number divisible by 3 and 5', () => {
+        expect(myFizzBuzz(15)).toBe("fizzbuzz");
+    });
+    it('Tests a number divisible only by 3', () => {
+        expect(myFizzBuzz(6)).toBe("fizz");
+    });
+    it('Tests a number divisible only by 5', () => {
+        expect(myFizzBuzz(10)).toBe("buzz");
+    });
+    it('Tests a number not divisible by 3 or 5', () => {
+        expect(myFizzBuzz(7)).toBe(7);
+    });
+    it('Tests not a number', () => {
+        expect(myFizzBuzz('7')).toBe(false);
+    });
+
+});
