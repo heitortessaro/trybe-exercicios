234a9a594ffca343128e9caa0e13ac5d80e4e827
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-01-16 20:55:58 -0300

    Funcao 2 exercicio 5. Finaliza exercicio 5.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/aula/exercicio-5.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/aula/exercicio-5.js
@@ -1,8 +1,28 @@
+habilities = ['HTML', 'CSS', 'JS', 'Git', 'Soft-skills'];
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
 const replaceX = (inputString, replacement) => {
     replacement = ` ${replacement} `
     return inputString.replace(' x ', replacement);
 }
 
+// https://www.w3schools.com/jsref/jsref_sort.asp
+const sortArray = textArray => {
+    return textArray.sort();
+}
 
-console.log(replaceX("Tryber x aqui!", "Bebeto"));
+const organizeString = (startString, replacementString, habilities) => {
+    const mainString = replaceX(startString, replacementString);
+    const sortedHabilities = sortArray(habilities);
+    return `${mainString}
+    - ${sortedHabilities[0]};
+    - ${sortedHabilities[1]};
+    - ${sortedHabilities[2]};
+    - ${sortedHabilities[3]};
+    - ${sortedHabilities[4]}.
+    #goTrybe
+    `;
+}
+
+console.log(organizeString('Tryber x aqui! Minhas cinco principais habilidades são:', 'Heitor', habilities));
 
