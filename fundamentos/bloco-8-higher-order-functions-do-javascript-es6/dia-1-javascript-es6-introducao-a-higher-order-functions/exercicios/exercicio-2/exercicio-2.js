5a5aaaf9de7810746e4ac71edea735a23fb69947
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-01-18 16:24:13 -0300

    Exercicio 2 - funcao checa e sorteio.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicio-2/exercicio-2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicio-2/exercicio-2.js
@@ -1,2 +1,11 @@
 const random = (max) => { return Math.floor(max * Math.random()) + 1 };
 
+const checaSorteio = (numEsc, numGan) => numEsc === numGan;
+
+const sorteio = (meuNumero, callback) => {
+    const numGanhador = random(5);
+    console.log(numGanhador);
+    return callback(meuNumero, numGanhador) ? 'Dia de sorte, você ganhou!' : 'Hoje não foi seu dia =/.'
+}
+
+console.log(sorteio(2, checaSorteio));
