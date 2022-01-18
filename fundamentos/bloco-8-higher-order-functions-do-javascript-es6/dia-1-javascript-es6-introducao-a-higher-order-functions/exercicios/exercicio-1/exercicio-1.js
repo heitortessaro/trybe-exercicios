091d6e0b5b0a5403be50b0b9b64b7994057b38d6
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-01-18 15:34:11 -0300

    Exercicio 1: refatora funcao retorna objeto.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicio-1/exercicio-1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/exercicio-1/exercicio-1.js
@@ -1,9 +1,19 @@
-const obj = (fullName, email) => {
+const obj = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
     const newObj = {
         nomeCompleto: fullName,
-        email: email,
+        email: `${email}@trybe.com`,
     };
     return newObj;
 }
 
-console.log(obj('Heitor José Tessaro', 'heitor.tessaro@hotmail.com'));
+const newEmployees = () => {
+    const employees = {
+        id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+        id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+        id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+};
+
+console.log(obj('Heitor Jose Tessaro'));
