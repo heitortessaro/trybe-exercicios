6ac233e5410f3a7bed45df87825f0e3250697c44
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-01-22 07:48:19 -0300

    Exemplo 1 - get the bigger number.

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exemplos/exemplo-1.js
@@ -0,0 +1,18 @@
+// Neste exemplo, serão comparados valores para buscar o maior valor em um array. Tente criar essa função apenas com for e depois tente com reduce . 
+
+const numbers = [50, 85, -30, 3, 15];
+
+const getBiggerFor = (arr) => {
+    let biggerNumber = arr[0];
+    for (let index = 1; index < arr.length; index += 1) {
+        arr[index] > biggerNumber ?
+            biggerNumber = arr[index] :
+            biggerNumber = biggerNumber;
+    }
+    return biggerNumber;
+}
+
+const getBigger = (acc, number) => acc > number ? acc : acc = number;
+
+console.log(`Com for ${getBiggerFor(numbers)}`);
+console.log(`Com reduce ${numbers.reduce(getBigger)}`);
