46cb6fafca967bdb1925410941e71a0c92e172b1
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-08 17:41:40 -0300

    Add function renderGenres.

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-8-semana-ontrack/projeto-SpotiTrybe/script.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-8-semana-ontrack/projeto-SpotiTrybe/script.js
@@ -2,6 +2,8 @@ const CLIENT_ID = '460f5f47bb564c1bbe6f61e0dd50407f';
 const CLIENT_SECRET = 'c82f485d94d0472b8c01c00e05dde9f0';
 const BASE_URL = 'https://accounts.spotify.com/';
 
+// REQUESTS
+
 const getToken = async () => {
     const requestInfo = {
         method: 'POST',
@@ -14,7 +16,7 @@ const getToken = async () => {
     const response = await fetch(`https://accounts.spotify.com/api/token`, requestInfo);
     const data = await response.json();
     return data.access_token;
-}
+};
 
 const getGenre = async (token) => {
     const requestInfo = {
@@ -27,11 +29,40 @@ const getGenre = async (token) => {
     const url = 'https://api.spotify.com/v1/browse/categories?locale=pt-br';
     const response = await fetch(url, requestInfo);
     const data = await response.json();
-    console.log(data);
-}
+    return data.categories.items;
+    // console.log(data);
+};
+
+// RENDERS
+
+const renderGenres = (genres) => {
+    const genresCards = document.querySelector('.genre-cards');
+
+    genres.forEach((genre) => {
+        const section = document.createElement('section');
+        section.className = 'genre';
+        section.id = genre.id;
+
+        const paragraph = document.createElement('p');
+        paragraph.className = 'genre-title';
+        paragraph.innerHTML = genre.name;
+
+        const image = document.createElement('img');
+        image.src = genre.icons[0].url;
+        image.className = 'genre-image';
+
+        section.appendChild(image);
+        section.appendChild(paragraph);
+        genresCards.appendChild(section);
+    });
+};
 
 window.onload = async () => {
-    token = await getToken();
-    await getGenre(token);
-    // console.log(token);
-}
+    try {
+        const token = await getToken();
+        const genres = await getGenre(token);
+        renderGenres(genres);
+    } catch (error) {
+        console.log(error);
+    }
+};
