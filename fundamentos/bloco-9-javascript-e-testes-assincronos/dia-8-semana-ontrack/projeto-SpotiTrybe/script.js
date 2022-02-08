e1c0578ed1a1227bff29c6a931d8bd396852dd30
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-08 17:06:36 -0300

    Complete fetch authorization fetch."

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-8-semana-ontrack/projeto-SpotiTrybe/script.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-8-semana-ontrack/projeto-SpotiTrybe/script.js
@@ -1,2 +1,21 @@
 const CLIENT_ID = '460f5f47bb564c1bbe6f61e0dd50407f';
-const CLIENT_SECRET = 'afc35823f8294d7fa0cfe0f844f55348';
+const CLIENT_SECRET = 'c82f485d94d0472b8c01c00e05dde9f0';
+const BASE_URL = 'https://accounts.spotify.com/';
+
+const getToken = async () => {
+    const response = await fetch(`https://accounts.spotify.com/api/token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+        },
+        body: 'grant_type=client_credentials',
+    });
+    const data = await response.json();
+    return data.access_token;
+}
+
+window.onload = async () => {
+    token = await getToken();
+    console.log(token);
+}
