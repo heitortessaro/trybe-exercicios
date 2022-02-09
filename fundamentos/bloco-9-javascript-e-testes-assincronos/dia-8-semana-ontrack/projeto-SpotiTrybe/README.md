48c524365e5bbcba31c960f5ca9c4106425266dc
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-09 16:35:21 -0300

    Pega cod final do repo. Add readme.

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-8-semana-ontrack/projeto-SpotiTrybe/README.md
@@ -0,0 +1,5 @@
+## SpotiTrybe
+
+Projeto desenvolvido durante a smeana OnTrack.
+
+O projeto utiliza a API do Spotify para fazer busca de playlists segundo o genero selecionado. O usuário pode então selecionar uma das playlists apresentadas de acordo com o genero. Selecionada a playlist, a página mostra as músicas disponíveis para o usuário, que pode selecionar para escutá-las parcialmente (30s).
--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-8-semana-ontrack/projeto-SpotiTrybe/script.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-8-semana-ontrack/projeto-SpotiTrybe/script.js
@@ -1,6 +1,76 @@
-const CLIENT_ID = '460f5f47bb564c1bbe6f61e0dd50407f';
-const CLIENT_SECRET = 'c82f485d94d0472b8c01c00e05dde9f0';
-const BASE_URL = 'https://accounts.spotify.com/';
+const CLIENT_ID = '4179f8989f4f401bb66f2bb0729e2bb7';
+const CLIENT_SECRET = 'c9748ab6075249adb588d0db77fc414d';
+let token;
+
+// HELPERS
+
+const getElementOrClosest = (sectionClass, target) =>
+    target.classList.contains(sectionClass)
+        ? target
+        : target.closest(sectionClass);
+
+const clearSelectedItem = (containerSelector) => {
+    const element = document.querySelector(`${containerSelector} .item-selected`);
+
+    if (element) {
+        element.classList.remove('item-selected');
+    }
+};
+
+const clearPlaylists = () => {
+    const element = document.querySelector('.playlist-cards');
+    element.innerHTML = '';
+};
+
+const clearTracks = () => {
+    const element = document.querySelector('.tracks-cards');
+    element.innerHTML = '';
+};
+
+// HANDLERS
+
+const handleGenreCardClick = async ({ target }) => {
+    const genreSection = getElementOrClosest('.genre', target);
+    const id = genreSection.id;
+
+    clearSelectedItem('.genre-cards');
+    genreSection.classList.add('item-selected');
+
+    const playlists = await getPlaylists(token, id);
+    clearPlaylists();
+    renderPlaylists(playlists);
+};
+
+const handlePlaylistCardClick = async ({ target }) => {
+    const playlistSection = getElementOrClosest('.playlist', target);
+    const id = playlistSection.id;
+
+    clearSelectedItem('.playlist-cards');
+    playlistSection.classList.add('item-selected');
+
+    const tracks = await getTracks(token, id);
+    clearTracks();
+    renderTracks(tracks);
+};
+
+const handleTrackCardClick = async ({ target }) => {
+    const trackSection = getElementOrClosest('.track', target);
+    const id = trackSection.id;
+
+    clearSelectedItem('.tracks-cards');
+    trackSection.classList.add('item-selected');
+
+    const player = document.querySelector('#player');
+    if (player) {
+        player.querySelector('source').src = trackSection.value;
+        player.load();
+    } else {
+        createPlayer(trackSection.value);
+    }
+
+    document.querySelector('#playing-music-name').innerHTML = target.innerText;
+
+};
 
 // REQUESTS
 
@@ -13,50 +83,55 @@ const getToken = async () => {
         },
         body: 'grant_type=client_credentials',
     };
-    const response = await fetch(`https://accounts.spotify.com/api/token`, requestInfo);
+
+    const response = await fetch('https://accounts.spotify.com/api/token', requestInfo);
     const data = await response.json();
     return data.access_token;
 };
 
-const getGenre = async (token) => {
+const getGenres = async (token) => {
     const requestInfo = {
         method: 'GET',
         headers: {
             Authorization: `Bearer ${token}`,
         }
-    }
+    };
 
     const url = 'https://api.spotify.com/v1/browse/categories?locale=pt-br';
     const response = await fetch(url, requestInfo);
     const data = await response.json();
     return data.categories.items;
-    // console.log(data);
 };
 
-// HELPERS
-
-const getElementOrClosest = (sectionClass, target) =>
-    target.classList.contains('sectionClass')
-        ? target
-        : target.closest(sectionClass);
-
-
-const clearSelectedItem = (containerSelector) => {
-    const element = document.querySelector(`${containerSelector} .item-selected`);
-    if (element) {
-        element.classList.remove('item-selected');
+const getPlaylists = async (token, genreId) => {
+    const requestInfo = {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
     };
+
+    const url = `https://api.spotify.com/v1/browse/categories/${genreId}/playlists`;
+    const response = await fetch(url, requestInfo);
+    const data = await response.json();
+    return data.playlists.items;
 }
 
+const getTracks = async (token, playlistId) => {
+    const requestInfo = {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    };
 
-//  HENDLERS
-
-const handleGenreCardClick = (event) => {
-    const genreSection = getElementOrClosest('.genre', event.target);
-    const id = genreSection.id;
-    clearSelectedItem('.genre-cards');
-    genreSection.classList.add('item-selected');
-};
+    const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
+    console.log(url);
+    const response = await fetch(url, requestInfo);
+    const data = await response.json();
+    console.log(data);
+    return data.items;
+}
 
 // RENDERS
 
@@ -72,11 +147,11 @@ const renderGenres = (genres) => {
         paragraph.className = 'genre-title';
         paragraph.innerHTML = genre.name;
 
-        const image = document.createElement('img');
-        image.src = genre.icons[0].url;
-        image.className = 'genre-image';
+        const img = document.createElement('img');
+        img.className = 'genre-image';
+        img.src = genre.icons[0].url;
 
-        section.appendChild(image);
+        section.appendChild(img);
         section.appendChild(paragraph);
         section.addEventListener('click', handleGenreCardClick);
 
@@ -84,12 +159,69 @@ const renderGenres = (genres) => {
     });
 };
 
+const renderPlaylists = (playlists) => {
+    const playlistsCards = document.querySelector('.playlist-cards');
+
+    playlists.forEach((playlist) => {
+        const section = document.createElement('section');
+        section.className = 'playlist text-card';
+        section.id = playlist.id;
+
+        const paragraph = document.createElement('p');
+        paragraph.className = 'playlist-title';
+        paragraph.innerHTML = playlist.name;
+
+        section.appendChild(paragraph);
+        section.addEventListener('click', handlePlaylistCardClick);
+
+        playlistsCards.appendChild(section);
+    });
+};
+
+const renderTracks = (tracks) => {
+    const tracksCards = document.querySelector('.tracks-cards');
+
+    tracks
+        .filter((track) => track.track && track.track.preview_url !== null)
+        .forEach((track) => {
+            const section = document.createElement('section');
+            section.className = 'track text-card';
+            section.id = track.id;
+            section.value = track.track.preview_url;
+
+            const paragraph = document.createElement('p');
+            paragraph.className = 'track-title';
+            paragraph.innerHTML = track.track.name;
+
+
+            section.appendChild(paragraph);
+            section.addEventListener('click', handleTrackCardClick);
+
+            tracksCards.appendChild(section);
+        });
+};
+
+const createPlayer = (src) => {
+    const audio = document.createElement('audio');
+    audio.controls = true;
+    audio.autoplay = true;
+    audio.id = 'player';
+
+    const source = document.createElement('source');
+    source.src = src;
+    audio.appendChild(source);
+
+    document.querySelector('.player-container').appendChild(audio);
+
+    return audio;
+};
+
 window.onload = async () => {
     try {
-        const token = await getToken();
-        const genres = await getGenre(token);
+        token = await getToken();
+        const genres = await getGenres(token);
         renderGenres(genres);
     } catch (error) {
         console.log(error);
     }
-};
+}
