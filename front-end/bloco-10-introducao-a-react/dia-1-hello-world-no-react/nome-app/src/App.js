fed54ad248e265dc92c298e9b43beced8576e983
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-16 18:51:19 -0300

    Cria Header.jsx

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+const atividades = ['Caminhar', 'Correr', 'Hiking', 'Walking'];
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Exercício 1.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ol>
+          {atividades.map((elemento) => Task(elemento))}
+        </ol>
       </header>
     </div>
   );
