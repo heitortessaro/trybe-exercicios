0a634d603aedcc1812ddd99e107a17f6341eddaf
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-22 18:50:33 -0300

    add inputs to FieldsetPersonalData

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/components/FieldsetPersonalData.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/components/FieldsetPersonalData.jsx
@@ -3,7 +3,33 @@ import React, { Component } from "react";
 class FieldsetPersonalData extends Component{
   render () {
     return (
-      <h1>Hello</h1>
+      <fieldset>
+        <legend>Personal Data</legend>
+        <div className="input-form">
+          {/* Todos os caracteres devem ser transformados para UPPER CASE assim que forem digitados.  */}
+          <label htmlFor="name">Name</label>
+          <input type="text" name='name' maxLength={40} required/>
+        </div>
+        <div className="input-form">
+          <label htmlFor="email">Email</label>
+          <input type='email' name='email' maxLength={50} required/>
+        </div>
+        <div className="input-form">
+          <label htmlFor="cpf">CPF</label>
+          <input type="text" name='cpf' maxLength={11} required/>
+        </div>
+        <div className="input-form">
+          {/* Remover qualquer caracter especial que seja digitado  */}
+          <label htmlFor="adress">Adress</label>
+          <input type="text" name='adress' maxLength={200} required/>
+        </div>
+        <div className="input-form">
+          {/* Ao remover o foco desse campo (evento onBlur ), verificar se o nome da cidade começa com números. Caso comece, limpar o campo.  */}
+          <label htmlFor="city">City</label>
+          <input type="text" name='City' maxLength={28} required/>
+        </div>
+
+      </fieldset>
     );
   }
 }
