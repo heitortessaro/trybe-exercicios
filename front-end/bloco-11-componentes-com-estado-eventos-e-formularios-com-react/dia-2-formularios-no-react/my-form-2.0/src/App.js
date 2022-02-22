ed3624a809c33e76bc78603d0bafd407f75b8e9f
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-22 18:30:49 -0300

    remove unnecessary files. Add FildsetPersonalData.

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -0,0 +1,11 @@
+import FieldsetPersonalData from "./components/FieldsetPersonalData";
+
+function App() {
+  return (
+    <div className="App">
+      <FieldsetPersonalData />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/components/FieldsetPersonalData.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class FieldsetPersonalData extends Component{
+  render () {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default FieldsetPersonalData;
