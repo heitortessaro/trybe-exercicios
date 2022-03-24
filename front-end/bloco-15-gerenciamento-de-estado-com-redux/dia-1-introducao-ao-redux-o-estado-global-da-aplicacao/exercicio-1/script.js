b60cdfa77f2cfbe0b6c2f1447feae4cc1e0fd5a0
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-03-24 19:03:27 -0300

    Cria projeto e instala redux

---
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercicio-1/script.js
@@ -0,0 +1,28 @@
+//    Definir o estado da aplicação
+//    INITIAL_STATE 
+const ESTADO_INICIAL = {
+  colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
+  index: 0,
+};
+//    Adicionar REDUX DEVTOOLS
+
+// <====== Lidando com o estado ======>
+// const CHANGE_THEME = 'CHANGE_THEME'
+//    Definir as mudanças do estado
+//    ACTIONS 
+
+//    Definir como o estado será atualizado
+//    REDUCER 
+// const reducer = (state = INITIAL_STATE, action) => { 
+// }
+
+//    Ligar nossa STORE ao Reducer
+// const store = Redux.createStore(
+//   reducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// <====== Alterando e Lendo o estado ======>
+
+//    Lendo o estado
+//    GETSTATE | SUBSCRIBE
