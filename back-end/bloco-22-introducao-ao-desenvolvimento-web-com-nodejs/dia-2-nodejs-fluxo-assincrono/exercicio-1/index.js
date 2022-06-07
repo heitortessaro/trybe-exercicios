27cff0ef13d3fb3de5bf42ba8bcdf86e4422d896
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-07 14:43:19 -0300

    Exercicio 3: usa async await.

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio-1/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio-1/index.js
@@ -4,3 +4,15 @@ const {randomNumber} = require('./randomNumber');
 trataNumeros(randomNumber('a'),randomNumber(100),randomNumber(100))
   .then(result => console.log(`Result: ${result}`))
   .catch(err => console.log(`error: ${err}`));
+
+
+async function teste(num1, num2, num3) {
+  try {
+    const result = await trataNumeros(num1, num2, num3);
+    console.log(`Result: ${result}`);
+  } catch (error) {
+    console.log(`error: ${error}`);
+  }
+}
+
+teste(randomNumber(10),randomNumber(10),randomNumber(10));
