8201165fdde5d19e6ff2814b76eabad60b42959f
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-10 15:54:51 -0300

    adiciona midleware para printar no console os dados da requisição.

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exemplo-2/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exemplo-2/index.js
@@ -5,6 +5,16 @@ const authMiddleware = require('./auth-middleware');
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, _res, next) => {
+  console.log('req.method:', req.method);
+  console.log('req.path:', req.path);
+  console.log('req.params:', req.params);
+  console.log('req.query:', req.query);
+  console.log('req.headers:', req.headers);
+  console.log('req.body:', req.body);
+  next();
+});
+
 // Esta rota não passa pelo middleware de autenticação!
 app.get('/open', function (req, res) {
   res.send('open!')
