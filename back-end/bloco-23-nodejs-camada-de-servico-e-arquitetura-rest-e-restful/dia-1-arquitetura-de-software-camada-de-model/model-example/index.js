62e0fb784b18d88939a4dcb21e1d170609d9976b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-23 15:48:31 -0300

    Add route /authors to return the info about all authors.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const Author = require('./models/Author');
 
 const app = express();
 
+// utiliza a porta definida na variável global ou então a 3000
 const PORT = process.env.PORT || 3000;
 
+// rota de teste
 app.get('/test', (_req, res) => {
   res.status(200).json('message: tudo certo')
 })
+
+// rota para retornar all authors
+app.get('/authors', async (_req, res) => {
+	const authors = await Author.getAll();
+
+	res.status(200).json(authors);
+});
 
 app.listen(PORT, () => {
 	console.log(`Ouvindo a porta ${PORT}`);
