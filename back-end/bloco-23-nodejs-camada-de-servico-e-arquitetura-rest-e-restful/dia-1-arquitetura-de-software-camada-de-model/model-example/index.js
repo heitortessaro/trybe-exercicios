14eb754ffc8f4a4a3187b7782894f197f4394ac7
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-23 19:25:20 -0300

    Create root to post books /books.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -70,6 +70,20 @@ app.get('/books/:id', async (req, res) => {
 	res.status(200).json(book);
 });
 
+app.post('/books', async (req, res) => {
+	const { title, author_id } = req.body;
+
+	if (!Book.isValid(title, author_id)) {
+		return res.status(400).json({ message: 'Dados inválidos' });
+	}
+
+	await Book.create(title, author_id);
+
+	res.status(201).json({ message: 'Livro criado com sucesso!'});
+});
+
+//-------------------------------------
+
 app.listen(PORT, () => {
 	console.log(`Ouvindo a porta ${PORT}`);
 });
