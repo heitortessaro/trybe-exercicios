1cbc83fdd8e8d4c821343537ff6f66f233154545
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-23 15:57:36 -0300

    Create function findById to return data about an author.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
@@ -33,6 +33,29 @@ const getAll = async () => {
 	return authors.map(serialize);;
 };
 
+const findById = async (id) => {
+	// Repare que substituímos o id por `?` na query.
+	// Depois, ao executá-la, informamos um array com o id para o método `execute`.
+	// O `mysql2` vai realizar, de forma segura, a substituição do `?` pelo id informado.
+	const query = 'SELECT first_name, middle_name, last_name FROM model_example.authors WHERE id = ?'
+	const response = await connection.execute(query, [id]);
+  console.log(response);
+  const [ authorData ] = response;
+  console.log(authorData);
+	if (authorData.length === 0) return null;
+
+	// Utilizamos [0] para buscar a primeira linha, que deve ser a única no array de resultados, pois estamos buscando por ID.
+	const { firstName, middleName, lastName } = authorData.map(serialize)[0];
+
+	return getNewAuthor({
+		id,
+		firstName,
+		middleName,
+		lastName,
+	});
+};
+
 module.exports = {
 	getAll,
+  findById,
 };
