5029735b58a426a04f2383cd584b6e1f178725e4
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-23 15:43:45 -0300

    Add nodemon and create a route test to test the system.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/test', (_req, res) => {
+  res.status(200).json('message: tudo certo')
+})
+
 app.listen(PORT, () => {
 	console.log(`Ouvindo a porta ${PORT}`);
 });
