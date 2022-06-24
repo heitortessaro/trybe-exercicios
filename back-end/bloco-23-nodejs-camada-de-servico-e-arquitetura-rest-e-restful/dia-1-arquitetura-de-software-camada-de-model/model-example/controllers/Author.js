dce8d27d3e2eaebf352eda9ea174097d591b1f7d
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-24 16:55:48 -0300

    Add validation against error from service at the controller findById.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/controllers/Author.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/controllers/Author.js
@@ -11,9 +11,12 @@ const findById = async (req, res) => {
 
   const author = await Author.findById(id);
 
-  if (!author) return res.status(404).json({ message: 'Author not found' });
+// Caso o service retorne um erro, interrompemos o processamento
+  // e inicializamos o fluxo de erro
+  if (author.error) return next(author.error);
 
-  res.status(200).json(author);
+  // Caso não haja nenhum erro, retornamos o author encontrado
+  return res.status(200).json(author);
 };
 
 const createAuthor = async (req, res) => {
