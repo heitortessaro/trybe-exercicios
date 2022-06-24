a1ce926a552bc2f95fba629cf4e0ed9f83f0b617
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-24 15:55:33 -0300

    Change index.js. Now it uses services author, and the validation of post is done by the service.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const Author = require('./models/Author');
+// const Author = require('./models/Author');
+const Author = require('./services/Author');
 const Book = require('./models/Book');
 
 const app = express();
@@ -10,80 +11,86 @@ const PORT = process.env.PORT || 3000;
 
 // rota de teste
 app.get('/test', (_req, res) => {
-  res.status(200).json('message: tudo certo')
-})
+  res.status(200).json('message: tudo certo');
+});
 
 // -------- Auhors -------------------
 
 // rota para retornar all authors
 app.get('/authors', async (_req, res) => {
-	const authors = await Author.getAll();
+  const authors = await Author.getAll();
 
-	res.status(200).json(authors);
+  res.status(200).json(authors);
 });
 
 // retorna dados de um author
 app.get('/authors/:id', async (req, res) => {
-	const { id } = req.params;
+  const {id} = req.params;
 
-	const author = await Author.findById(id);
+  const author = await Author.findById(id);
 
-	if (!author) return res.status(404).json({ message: 'Not found' });
+  if (!author) return res.status(404).json({message: 'Not found'});
 
-	res.status(200).json(author);
+  res.status(200).json(author);
 });
 
 app.post('/authors', async (req, res) => {
-	const { first_name, middle_name, last_name } = req.body;
+  const {first_name, middle_name, last_name} = req.body;
 
-	if (!Author.isValid(first_name, middle_name, last_name)) {
-		return res.status(400).json({ message: 'Dados inválidos' });
-	}
+  // if (!Author.isValid(first_name, middle_name, last_name)) {
+  // 	return res.status(400).json({ message: 'Dados inválidos' });
+  // }
 
-	await Author.create(first_name, middle_name, last_name);
+  // await Author.create(first_name, middle_name, last_name);
 
-	res.status(201).json({ message: 'Autor criado com sucesso! '});
+  // res.status(201).json({ message: 'Autor criado com sucesso! '});
+
+  const author = await Author.createAuthor(first_name, middle_name, last_name);
+
+  if (!author) return res.status(400).json({message: 'Dados inválidos'});
+
+  res.status(201).json(author);
 });
 
 // -------- books -------------------
 
-app.get('/books', async (_req,res) => {
+app.get('/books', async (_req, res) => {
   const books = await Book.getAll();
 
   res.status(200).json(books);
-})
+});
 
-app.get('/books/author/:id', async(req, res) => {
-  const { id } = req.params;
+app.get('/books/author/:id', async (req, res) => {
+  const {id} = req.params;
   const books = await Book.findByAuthorId(id);
-  if (!books) return res.status(404).json({ message: 'Not found'});
+  if (!books) return res.status(404).json({message: 'Not found'});
   res.status(200).json(books);
-})
+});
 
 app.get('/books/:id', async (req, res) => {
-	const { id } = req.params;
+  const {id} = req.params;
 
-	const book = await Book.findById(id);
+  const book = await Book.findById(id);
 
-	if (!book) return res.status(404).json({ message: 'Not found' });
+  if (!book) return res.status(404).json({message: 'Not found'});
 
-	res.status(200).json(book);
+  res.status(200).json(book);
 });
 
 app.post('/books', async (req, res) => {
-	const { title, author_id } = req.body;
+  const {title, author_id} = req.body;
 
-	if (!Book.isValid(title, author_id)) {
-		return res.status(400).json({ message: 'Dados inválidos' });
-	}
+  if (!Book.isValid(title, author_id)) {
+    return res.status(400).json({message: 'Dados inválidos'});
+  }
 
-	await Book.create(title, author_id);
+  await Book.create(title, author_id);
 
-	res.status(201).json({ message: 'Livro criado com sucesso!'});
+  res.status(201).json({message: 'Livro criado com sucesso!'});
 });
 
 //-------------------------------------
 
 app.listen(PORT, () => {
-	console.log(`Ouvindo a porta ${PORT}`);
+  console.log(`Ouvindo a porta ${PORT}`);
 });
