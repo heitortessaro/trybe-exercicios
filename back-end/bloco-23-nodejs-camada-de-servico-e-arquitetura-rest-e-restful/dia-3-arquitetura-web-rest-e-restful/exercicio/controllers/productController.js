06b6c70a91df4235fd062c6dab1d83205cc95554
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-27 18:27:31 -0300

    Altera o status code das respostas para opções mais adequadas e altera a forma da resposta para sempre que necessário incluir as informações requisitadas na formato JSON.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio/controllers/productController.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio/controllers/productController.js
@@ -6,35 +6,68 @@ const router = express.Router();
 router.get('/products', async (req, res) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
 router.get('/products/:id', async (req, res) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  if (product === null) {
+    return res.status(404).json({ message: 'Produto não encontrado' });
+  }
+
+  res.status(200).json(product);
 });
 
 router.post('/products', async (req, res) => {
   const { name, brand } = req.body;
 
+  if (name === undefined || brand === undefined) {
+    return res.status(400).json({ message: 'Informações do produto inválidas' });
+  }
+
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  // code 201 para indicar que o produto foi criado
+  res.status(201).json(newProduct);
 });
 
 router.delete('/products/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+  const product = await ProductModel.getById(req.params.id);
 
-  res.send(products);
+  if (product === null) {
+    // status code 404 para indicar
+    // que o recurso não foi encontrado 
+    return res.status(404).json({ message: 'Produto não encontrado' });
+  }
+
+  await ProductModel.exclude(req.params.id);
+
+  // status code 204 para indicar que a requisição foi concluída
+  // com sucesso e seu retorno não possuirá conteúdo.
+  res.status(204).json();
 });
+
+// The 404 represent resource not exist. 
+// i.e The called API dose not exist or Request Page dose not exist.
+// But in case 400 the resource exist but input is wrong.
+
 
 router.put('/products/:id', async (req, res) => {
   const { name, brand } = req.body;
 
-  const products = await ProductModel.update(req.params.id, name, brand);
+  if (name === undefined || brand === undefined) {
+    return res.status(400).json({ message: 'Informações do produto inválidas' });
+  }
 
-  res.send(products);
+  const product = await ProductModel.getById(req.params.id);
+  if (product === null) {
+    return res.status(404).json({ message: 'Produto não encontrado' });
+  }
+
+  await ProductModel.update(req.params.id, name, brand);
+
+  res.status(200).json({ name, brand });
 });
 
 module.exports = router;
