63045018f7c32df9b8fc568bf1f2ba24a00fe166
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-28 18:48:47 -0300

    feat: add route to get movies by id to the index.js.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/index.js
@@ -9,6 +9,8 @@ app.use(bodyParser.json());
 
 app.post('/movies', MovieController.create);
 
+app.get('/movies/:id', MovieController.findById);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
