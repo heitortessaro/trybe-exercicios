183bd55acc0fb6175f1bd896df1611364c5b62fc
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-28 17:14:41 -0300

    test: Add test for id request for the model Add tests to validate the operation of the moviesModel when a resquest by id is done.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/tests/models/movieModels.test.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/tests/models/movieModels.test.js
@@ -34,5 +34,61 @@ describe('Insere um novo filme no BD', () => {
       expect(response).to.have.a.property('id')
     });
 
+  });  
+});
+
+describe('Busca apenas um filme no BD por seu ID', () => {
+  before(async () => {
+    const execute = [[]];
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  describe('quando não existe um filme com o ID informado', () => {
+    it('retorna null', async () => {
+      const response = await MoviesModel.getById();
+      expect(response).to.be.equal(null);
+    });
+  });
+
+  describe('quando existe um filme com o ID informado', () => {
+
+    before(() => {
+      sinon.stub(MoviesModel, 'getById')
+        .resolves(
+          {
+            id: 1,
+            title: 'Example Movie',
+            directedBy: 'Jane Dow',
+            releaseYear: 1999,
+          }
+        );
+    });
+
+    after(() => {
+      MoviesModel.getById.restore();
+    });
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesModel.getById(1);
+
+      expect(response).to.be.an('object');
+    });
+
+    it('o objeto não está vazio', async () => {
+      const response = await MoviesModel.getById(1);
+
+      expect(response).to.be.not.empty;
+    });
+
+    it('tal objeto possui as propriedades: "id", "title", "releaseYear" e "directedBy"', async () => {
+      const item = await MoviesModel.getById(1);
+
+      expect(item).to.include.all.keys('id', 'title', 'releaseYear', 'directedBy');
+    });
   });
 });
