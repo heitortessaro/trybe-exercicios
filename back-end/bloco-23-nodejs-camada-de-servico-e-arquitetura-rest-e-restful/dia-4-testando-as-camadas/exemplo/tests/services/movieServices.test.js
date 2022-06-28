db7777c1f97c5876a23a3bf6a4594a5a63a533b7
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-28 17:19:29 -0300

    test: Create tests to validade movieServices when a request to get a movie by an Id is done.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/tests/services/movieServices.test.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/tests/services/movieServices.test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const connection = require('../../models/connection');
 
 const MoviesService = require('../../services/movieServices');
 const MoviesModel = require('../../models/movieModels');
@@ -56,5 +57,62 @@ describe('Insere um novo filme no BD', () => {
       expect(response).to.have.a.property('id');
     });
 
+  });
+});
+
+describe('Busca apenas um filme no BD por seu ID', () => {
+  before(async () => {
+    const execute = [[]];
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  describe('quando não existe um filme com o ID informado', () => {
+    it('retorna null', async () => {
+      const response = await MoviesService.findById();
+
+      expect(response).to.be.equal(null);
+    });
+  });
+
+  describe('quando existe um filme com o ID informado', () => {
+
+    before(() => {
+      sinon.stub(MoviesModel, 'getById')
+        .resolves(
+          {
+            id: 1,
+            title: 'Example Movie',
+            directedBy: 'Jane Dow',
+            releaseYear: 1999,
+          }
+        );
+    });
+
+    after(() => {
+      MoviesModel.getById.restore();
+    });
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesService.findById(1);
+
+      expect(response).to.be.an('object');
+    });
+
+    it('o objeto não está vazio', async () => {
+      const response = await MoviesService.findById(1);
+
+      expect(response).to.be.not.empty;
+    });
+
+    it('tal objeto possui as propriedades: "id", "title", "releaseYear" e "directedBy"', async () => {
+      const item = await MoviesService.findById(1);
+
+      expect(item).to.include.all.keys('id', 'title', 'releaseYear', 'directedBy');
+    });
   });
 });
