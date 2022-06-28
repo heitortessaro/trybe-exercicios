2337e5738174c8f8e672d7d5b8f22ab47bcffda8
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-28 15:30:53 -0300

    chore: add comments to explain the movieController test.

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/tests/controllers/movieControllers.test.js
@@ -0,0 +1,68 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const MoviesController = {
+  create: () => {}
+};
+
+describe('Ao chamar o controller de create', () => {
+  describe('quando o payload informado não é válido', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.body = {};
+
+      response.status = sinon.stub()
+        .returns(response);
+      response.send = sinon.stub()
+        .returns();
+    })
+
+    it('é chamado o status com o código 400', async () => {
+      await MoviesController.create(request, response);
+
+      // Criamos stubs específicos para simular funções de resposta (response). Assim, conseguimos utilizar o método calledWith fornecido pelo Sinon para testarmos se a função foi chamada com os parâmetros esperados.
+      // Por exemplo: no trecho de código abaixo, validamos se o método res.status (ou response.status) foi chamado passando o status code 400. Lembre-se que é dessa forma que nossa API responde à requisição da pessoa usuária, utilizando API's REST com frameworks de middleware, como o express.
+      expect(response.status.calledWith(400)).to.be.equal(true);
+    });
+
+    it('é chamado o send com a mensagem "Dados inválidos"', async () => {
+      await MoviesController.create(request, response);
+
+      expect(response.send.calledWith('Dados inválidos')).to.be.equal(true);
+    });
+
+  });
+
+  describe('quando é inserido com sucesso', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.body = {
+        title: 'Example Movie',
+        directedBy: 'Jane Dow',
+        releaseYear: 1999,
+      };
+
+      response.status = sinon.stub()
+        .returns(response);
+      response.send = sinon.stub()
+        .returns();
+    })
+
+    it('é chamado o status com o código 201', async () => {
+      await MoviesController.create(request, response);
+
+      expect(response.status.calledWith(201)).to.be.equal(true);
+    });
+
+    it('é chamado o send com a mensagem "Filme criado com sucesso!"', async () => {
+      await MoviesController.create(request, response);
+
+      expect(response.send.calledWith('Filme criado com sucesso!')).to.be.equal(true);
+    });
+
+  });
+});
