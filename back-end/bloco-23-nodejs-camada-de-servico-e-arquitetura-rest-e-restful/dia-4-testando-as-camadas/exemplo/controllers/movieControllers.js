e1dfbf78f54af1fbb1d4a6ba1bb7f32888b3388e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-28 18:47:31 -0300

    feat: add function getById to the movie controller after have created the tests for it.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/controllers/movieControllers.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/controllers/movieControllers.js
@@ -26,6 +26,20 @@ const create = async (req, res) => {
     .send('Filme criado com sucesso!');
 };
 
+const findById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await MoviesService.findById(id);
+    if (!movie) {
+      return res.status(404).send('Not Found');
+    }
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   create,
+  findById,
 };
