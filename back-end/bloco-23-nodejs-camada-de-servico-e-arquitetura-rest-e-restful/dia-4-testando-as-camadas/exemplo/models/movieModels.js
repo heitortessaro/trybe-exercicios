25e7caeea6647332c89e730537b7404ef45616d6
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-28 17:16:56 -0300

    feat: Create function to the movieMovel get information about a movie using an Id.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/models/movieModels.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exemplo/models/movieModels.js
@@ -12,6 +12,25 @@ const create = async ({ title, directedBy, releaseYear }) => {
   };
 };
 
+const getById = async (id) => {
+  const query = 'SELECT * FROM model_example.movies WHERE id = ?';
+
+  const [movie] = await connection.execute(query, [id]);
+
+  if (movie.length === 0) return null;
+
+  const { title, directed_by: directedBy, release_year: releaseYear } = movie[0];
+
+  return {
+    id,
+    title,
+    directedBy,
+    releaseYear,
+  };
+};
+
+
 module.exports = {
   create,
+  getById,
 };
