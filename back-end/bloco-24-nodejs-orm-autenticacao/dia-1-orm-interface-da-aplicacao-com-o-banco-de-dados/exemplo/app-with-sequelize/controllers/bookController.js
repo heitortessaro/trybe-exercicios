128a835d00a14c220030db250c9f8af16ce54a24
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-07 19:11:17 -0300

    feat: :sparkles: Create getById bookService and bookController

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo/app-with-sequelize/controllers/bookController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo/app-with-sequelize/controllers/bookController.js
@@ -7,5 +7,12 @@ router.get('/', async (_req, res) => {
   res.status(200).json(books);
 });
 
+const getById = async (req, res) => {
+  const book = await BookService.getById(req.params.id);
+
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+
+  res.status(200).json(book);
+};
 
 module.exports = router;
