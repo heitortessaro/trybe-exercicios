d00677661a84399bdce4f72712cd23e809645e3a
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-07 19:12:26 -0300

    fix: :bug: Change the getById bookController to use router

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo/app-with-sequelize/controllers/bookController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo/app-with-sequelize/controllers/bookController.js
@@ -7,12 +7,12 @@ router.get('/', async (_req, res) => {
   res.status(200).json(books);
 });
 
-const getById = async (req, res) => {
+router.get('/:id', async (req, res) => {
   const book = await BookService.getById(req.params.id);
 
   if (!book) return res.status(404).json({ message: 'Book not found' });
 
   res.status(200).json(book);
-};
+});
 
 module.exports = router;
