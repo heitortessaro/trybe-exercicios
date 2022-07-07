91e33931ae3d4e412126b36490056a548e716faa
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-07 17:27:30 -0300

    feat: :sparkles: Install express and nodemon. Move files to the project folder.
    In addition, the user model is now exported.

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo/app-with-sequelize/models/user.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo/app-with-sequelize/models/user.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
   fullName: DataTypes.STRING,
@@ -8,3 +10,5 @@ const User = (sequelize, DataTypes) => {
 
   return User;
 }
+
+module.exports = User
