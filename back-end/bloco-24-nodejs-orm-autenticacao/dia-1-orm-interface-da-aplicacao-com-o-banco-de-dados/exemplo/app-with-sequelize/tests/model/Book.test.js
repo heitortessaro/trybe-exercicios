e7c13edbf47a732428901eff1b8caae4fea8191c
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-08 19:22:46 -0300

    test: :white_check_mark: Install Chai and sequelize-test-helpers. Add tests to the Book model.

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo/app-with-sequelize/tests/model/Book.test.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} = require('sequelize-test-helpers');
+
+const BookModel = require('../../models/book')
+
+describe('O model de Book', () => {
+  const Book = BookModel(sequelize, dataTypes);
+  const book = new Book();
+
+  describe('possui o nome "Book"', () => {
+    checkModelName(Book)('Book');
+  });
+
+  describe('possui as propriedades "title", "author", "pageQuantity" e "publisher"', () => {
+    ['title', 'author', 'pageQuantity', 'publisher'].forEach(checkPropertyExists(book));
+  });
+});
