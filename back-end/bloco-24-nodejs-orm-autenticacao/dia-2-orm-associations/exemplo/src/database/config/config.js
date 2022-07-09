c365e27a11234586df84494a80828f2bc95a7d27
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-09 10:07:40 -0300

    fix: :bug: Replace global variables to local ones in config.js
    dotenv is not working and I dont know why.

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo/src/database/config/config.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo/src/database/config/config.js
@@ -1,25 +1,36 @@
-require('dotenv').config();
-
+// require('dotenv').config();
+// import 'dotenv/config';
+// const path = require('path')
+// require('dotenv').config({path: path.resolve(__dirname, '../../../.env')});
+// console.log(process.env.MYSQL_USER);
+// console.log(path.resolve(__dirname, '../../../.env'));
 module.exports = {
-  development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
+  "development": {
+    // "username": process.env.MYSQL_USER,
+    // "password": process.env.MYSQL_PASSWORD,
+    // "database": process.env.MYSQL_DATABASE,
+    // "host": process.env.HOSTNAME,
+    // "port": process.env.MYSQL_PORT,
+    // "dialect": process.env.MYSQL_DIALECT,
+    "username": "root",
+    "password": "root",
+    "database": "exemplo_24_2",
+    "host": "localhost",
+    "port": 3306,
+    "dialect": "mysql",
   },
-  test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
+  "test": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "dialect": process.env.MYSQL_DIALECT,
   },
-  production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-  },
-};
+  "production": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "dialect": process.env.MYSQL_DIALECT,
+  }
+}
